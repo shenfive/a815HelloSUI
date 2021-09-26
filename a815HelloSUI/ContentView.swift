144fe6257c3d83ba8b58b5b982317d051d0ef9f7
Danny Shen <shenfive@hotmail.com>
2021-09-26 15:05:53 +0800

    Color

--- a815HelloSUI/ContentView.swift
+++ a815HelloSUI/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var inputString:String = ""
+    @State var r:CGFloat = 0.5
+    @State var g:CGFloat = 0.5
+    @State var b:CGFloat = 0.5
+    
     var body: some View {
-        VStack{
-            Text(inputString)
-            Spacer()
-            Text("==分割線==")
-            HStack{
-                Text("請輸入：")
-                TextField("", text: $inputString)
+        ZStack{
+            Color(CGColor(red: r, green: g, blue: b, alpha: 1))
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("R:\( Int(r * 255) )").font(.largeTitle)
+                    Spacer()
+                    Text("G:\( Int(g * 255) )").font(.largeTitle)
+                    Spacer()
+                    Text("B:\( Int(b * 255) )").font(.largeTitle)
+                    Spacer()
+                }
+                Spacer()
+                VStack{
+                    Slider(value: $r).padding(.horizontal, 30)
+                    Slider(value: $g).padding(.horizontal, 30)
+                    Slider(value: $b).padding(.horizontal, 30)
+                    
+                }
             }
+            
         }
+        
     }
 }
 
